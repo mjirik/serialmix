4aa606f3c3a04135db7b6135926f7aab08ae6d01
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-12-16 22:21:00 +0100

    first rest api and serial port

---
+++ tests/test_rest.py
@@ -0,0 +1,26 @@
+import pytest
+import webapp
+import json
+
+
+# def pytest_sessionstart(session):
+#     """
+#     Called after the Session object has been created and
+#     before performing collection and entering the run test loop.
+#     """
+#     session.app = app.test_client()
+
+
+# @pytest.fixture(scope="session", autouse=True)
+def test_successful_signup():
+    app = webapp.app.test_client()
+    # Given
+    payload = json.dumps({
+        "email": "paurakh011@gmail.com",
+        "password": "mycoolpassword"
+    })
+
+    # When
+    response = app.post('/api/auth/signup', headers={"Content-Type": "application/json"}, data=payload)
+
+
---
+++ webapp.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+serial_port_0 = "/dev/ttyUSB0"
+serial_port_1 = "/dev/ttyUSB1"
+
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
+
+@app.route('/set_serial_device')
+def set_serial_port():
+    """
+    Set port of device 0 or 1
+    :return:
+    """
+    if request.method == "POST":
+        id = request.args.get("id")
+        port = request.args.get("port")
+        if id == 0:
+            serial_port_0 = port
+        else:
+            serial_port_1 = port
+
+
+filename = request.args.get("message")
+
+
+@app.route('/send_message')
+def send_message():
+    if request.method == "POST":
+        filename = request.args.get("message")
+        exists = Path(filename).exists()
+        with serial.Serial('/dev/ttyS1', 19200, timeout=1) as ser:
+            x = ser.read()  # read one byte
+            s = ser.read(10)  # read up to ten bytes (timeout)
+            line = ser.readline()  # read a '\n' terminated line
